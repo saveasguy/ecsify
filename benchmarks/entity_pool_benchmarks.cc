aced4d6af1266fa7421413b22a911062f251397d
Aleksei Romanov <raa_2001@mail.ru>
2025-03-05 13:35:08 +0300

    Introduce benchmarking

---
+++ benchmarks/entity_pool_benchmarks.cc
@@ -0,0 +1,54 @@
+#include <benchmark/benchmark.h>
+
+#include <ranges>
+
+#include "ecsify/internal/entity_pool.h"
+
+constexpr benchmark::IterationCount kMaxIterations = 1000000;
+
+static void BM_FillColdEntityPool(benchmark::State &state) {
+  ecsify::internal::EntityPool pool(0);
+  for (auto _ : state) {
+    pool.Add();
+  }
+}
+BENCHMARK(BM_FillColdEntityPool)->Iterations(kMaxIterations)->Repetitions(4);
+
+static void BM_FillWarmEntityPool(benchmark::State &state) {
+  ecsify::internal::EntityPool pool(0);
+  std::vector<ecsify::Entity> entities;
+  entities.reserve(state.max_iterations);
+  for (auto _ : std::views::iota(0, state.max_iterations)) {
+    entities.push_back(pool.Add());
+  }
+  for (ecsify::Entity entity : entities) {
+    pool.Remove(entity);
+  }
+  for (auto _ : state) {
+    pool.Add();
+  }
+}
+BENCHMARK(BM_FillWarmEntityPool)->Iterations(kMaxIterations)->Repetitions(4);
+
+static void BM_EntityPoolRemove(benchmark::State &state) {
+  ecsify::internal::EntityPool pool(0);
+  std::vector<ecsify::Entity> entities;
+  entities.reserve(state.max_iterations);
+  for (auto _ : std::views::iota(0, state.max_iterations)) {
+    entities.push_back(pool.Add());
+  }
+  for (auto _ : state) {
+    pool.Remove(entities.back());
+    entities.pop_back();
+  }
+}
+BENCHMARK(BM_EntityPoolRemove)->Iterations(kMaxIterations)->Repetitions(4);
+
+static void BM_EntityPoolAddRemove(benchmark::State &state) {
+  ecsify::internal::EntityPool pool(0);
+  for (auto _ : state) {
+    ecsify::Entity entity = pool.Add();
+    pool.Remove(entity);
+  }
+}
+BENCHMARK(BM_EntityPoolAddRemove)->Iterations(kMaxIterations)->Repetitions(4);
